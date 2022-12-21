993ef43cb686bafbeac0609841091d330bbd208c
gweryf <priyam2007sp@gmail.com>
2022-12-21 10:35:40 +0530

    added intial elements

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{const n=document.getElementById("content"),d=document.createElement("nav");d.classList.add("navbar"),d.innerHTML="\n    <p>Food Pad</p>\n    <div>\n        <div>Home</div>\n        <div>Menu</div>\n        <div>About</div>\n    </div>\n",n.appendChild(d)})();
